4065f60652ee094788e3518f76a4bcb0f0e60e3a
Yazeed Abusultan <yazeedsn153@outlook.com>
2024-08-29 14:19:38 +0300

    perf: use MobileNetv3 architecture to improve inference speed. perf: replace previous dataset with CIFAKE achieving better generalization. perf: resize to 32x32 input image for faster inference.

--- config.py
+++ config.py
@@ -3,25 +3,17 @@ sys.path.append(os.path.abspath('C:/Users/Yazeed/Desktop/workspace/flexaibuild')
 from functools import partial
 
 import torch
-from torch.nn import Sequential, Dropout, BatchNorm2d
-from torchvision.models import shufflenet_v2_x2_0, ShuffleNet_V2_X2_0_Weights
+from torch.nn import Sequential, Dropout, Linear
+from torchvision.models import mobilenet_v3_small, MobileNet_V3_Small_Weights
 from torchvision.transforms import Resize, ToTensor, Compose
-from flexai.modules import DenseLayer # type: ignore
 
 
 # DEFAULT MODEL
-MODEL = Sequential(
-  BatchNorm2d(3),
-  shufflenet_v2_x2_0(weights=ShuffleNet_V2_X2_0_Weights.DEFAULT),
+MODEL = mobilenet_v3_small(weights= MobileNet_V3_Small_Weights.DEFAULT)
+MODEL.classifier[-1] = Sequential(
+    Dropout(),
+    Linear(MODEL.classifier[-1].in_features, 2)
 )
-
-SHUFFLENET = MODEL[1]
-
-SHUFFLENET.fc = Sequential(
-  Dropout(0.7),
-  DenseLayer(SHUFFLENET.fc.in_features, 2, act=None),
-)
-
 
 
 # trained weights path
@@ -30,10 +22,10 @@ WEIGTHS_PATH = "weights/best_weights.pt"
 # Inference Transform
 INPUT_TRANSFORM = Compose([
   ToTensor(),
-  Resize((224, 224)),
+  Resize((32, 32)),
   partial(torch.unsqueeze, dim=0),
   ],
 )
 
 # label mapping
-IDX_TO_CLASS = {0: 'AI', 1: 'Real'}
+IDX_TO_CLASS = {0: 'FAKE', 1: 'REAL'}
